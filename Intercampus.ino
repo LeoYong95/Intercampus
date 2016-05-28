c1a6947fdd73d30a96174995ed73805f6cc63023
leoyong <y_c_l__@live.com>
2016-05-29 00:03:27 +0800

    test Bluetooth connection

--- Intercampus.ino
+++ Intercampus.ino
@@ -1,10 +1,30 @@
+int ledPin = 5;
+String readString;
+
+
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(12500);
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
+  while (Serial.available()) {
+    delay(3);
+    char c = Serial.read();
+    readString += c;
+  }
+  
+  if (readString.length() >0) {
+    Serial.println(readString);
+    if(readString == "on"){
+      digitalWrite(ledPin,HIGH);
+      
+    }else if(readString == "off"){
+      digitalWrite(ledPin,LOW);
+    }
+  readString = "";  
+  }
+  
 }
