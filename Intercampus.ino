8c3ce6648270ee5d328343d7694e7016be14770c
leoyong <y_c_l__@live.com>
2016-06-04 15:51:16 +0800

    Adding servo test

--- Intercampus.ino
+++ Intercampus.ino
@@ -1,10 +1,17 @@
-int ledPin = 5;
+#include <Servo.h>
+Servo srv;
+int leftledPin = 3;
+int rightledPin = 6;
+int forwardledPin = 7;
 String readString;
 
 
 void setup() {
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
+  srv.attach(5);
+  pinMode(leftledPin, OUTPUT);
+  pinMode(rightledPin, OUTPUT);
+  pinMode(forwardledPin, OUTPUT);
 
 }
 
@@ -18,11 +25,26 @@ void loop() {
   
   if (readString.length() >0) {
     Serial.println(readString);
-    if(readString == "on"){
-      digitalWrite(ledPin,HIGH);
+
+    if(readString == "left"){
+      digitalWrite(leftledPin,HIGH);
+    }else if(readString == "right"){
+      digitalWrite(rightledPin,HIGH);
+    }else if(readString == "forward"){
+      digitalWrite(forwardledPin,HIGH);
+    }else if(readString == "leftoff"){
+      digitalWrite(leftledPin,LOW);
+    }else if(readString == "rightoff"){
+      digitalWrite(rightledPin,LOW);
+    }else if(readString == "forwardoff"){
+      digitalWrite(forwardledPin,LOW);
+    }
+
+    if (readString.length()> 3 ){
+      String srvVal = readString.substring(3);
+     srv.write(srvVal.toInt());
+
       
-    }else if(readString == "off"){
-      digitalWrite(ledPin,LOW);
     }
   readString = "";  
   }
